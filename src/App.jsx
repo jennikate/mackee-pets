ca8d8b74c9957d31749ac5605c76613a4247a930
JenniKate Wallace <jennikate@gmail.com>
2021-07-01 11:26:54 +0100

    Setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Ceralie from './pages/ceralie';
+import Home from './pages/home';
 
 const App = () => (
-  <h1>
-    React App
-  </h1>
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/ceralie">Ceralie</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/ceralie">
+          <Ceralie />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
---
+++ src/pages/ceralie.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Ceralie = () => (
+  <h1>Ceralie</h1>
+);
+
+export default Ceralie;
