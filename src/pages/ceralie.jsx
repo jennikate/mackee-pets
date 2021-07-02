9062070f6f3c9dc013ec908a79ea4275d372b428
JenniKate Wallace <jennikate@gmail.com>
2021-07-02 09:27:38 +0100

    Update to get correct token

--- src/pages/ceralie.jsx
+++ src/pages/ceralie.jsx
@@ -1,17 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-// CURRENT STATUS
-/*
- * getting access token via axios below returns an invalid token
- * doing it through postman using curl request
- * curl --location --request POST 'https://eu.battle.net/oauth/token' \
- * --header 'Authorization: Basic MzlhYWQ2ODY0MGMyNGRiN2I0NTU2NTFmNjg2YzM5NTc6ak5rdFE1U3Q0aDNSQjhIWHdDVXpseXNwcGk0czBrUmQ=' \
- * --header 'Content-Type: application/x-www-form-urlencoded' \
- * --data-urlencode 'grant_type=client_credentials'
- * returns a valid token which I've hardcoded below for now
- */
-
 const Ceralie = () => {
   /*
   * Call the battlenet API with my credentials
@@ -21,40 +10,61 @@ const Ceralie = () => {
   const [data, setData] = useState();
   const [token, setToken] = useState();
 
-  // setup credentials :: to move to config/env
+  // setup consts (to move to const file later)
   const blizzUrl = 'api.blizzard.com';
-  // const clientId = '';
-  // const clientSecret = '';
-  // setup region details :: to move to const
   const region = 'eu';
   const namespace = 'profile-eu';
   const locale = 'en_US';
 
-  // get access token :: failing to return a valid token atm
-  // const getAccessToken = async () => {
-  //   try {
-  //     const response = await axios.post('https://eu.battle.net/oauth/token', {}, {
-  //       auth: {
-  //         username: clientId,
-  //         password: clientSecret,
-  //       },
-  //       params: {
-  //         grant_type: 'client_credentials',
-  //         scope: 'wow.profile',
-  //       },
-  //     });
-  //     setToken(response.data.access_token);
-  //   } catch (error) {
-  //     console.log('error', error);
-  //   }
-  // };
+  /*
+   * Below is converted from the curl command provided on the battlenet site
+   * I get the Auth code via postman using the CURL script provided by Blizz
+   * This is my code and only changes when client/secret IDs change
+   * I need to
+   * - put the auth code into an env and call it
+   * - refactor the below to axios try/catch for consistency
+   * - move to a util file
+   * - store token in localStorage with expiry handling
+  */
+  const getAccessToken = () => {
+    const myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Basic ');
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append('grant_type', 'client_credentials');
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow',
+    };
+
+    fetch('https://us.battle.net/oauth/token', requestOptions)
+      .then((response) => response.text())
+      .then((result) => setToken(JSON.parse(result)))
+      .catch((error) => console.log('error', error));
+  };
+
+  /*
+   * Returns the details of my user
+   * this is a test call, all calls can be made
+   * using the same structure
+   * just update the url request
+  */
 
   const getUser = async () => {
+    const urlParts = 'wow/character/argent-dawn/ceralie/appearance';
     if (token) {
       try {
-        const response = await axios.get(`https://${region}.${blizzUrl}/profile/user/wow?namespace=${namespace}&locale=${locale}&access_token=${token}`);
-        console.log(response.data.wow_accounts[0].characters[0].name);
-        setData(response.data.wow_accounts[0].characters[0].name);
+        const response = await axios.get(`https://${region}.${blizzUrl}/profile/${urlParts}?namespace=${namespace}&locale=${locale}&access_token=${token.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${token.access_token}`,
+          },
+        });
+        setData(response.data.character.name);
+        // console.log(response.data)
       } catch (error) {
         console.error(error);
       }
@@ -68,15 +78,13 @@ const Ceralie = () => {
   }, [token]);
 
   useEffect(() => {
-    // getAccessToken();
-    setToken('USMqt7HOnbE07xB008gEyWo0kLcY9lJny6');
+    getAccessToken();
   }, []);
 
   if (!data) { return null; }
   return (
     <>
-      <h1>Ceralie</h1>
-      <p>{data}</p>
+      <h1>{data}</h1>
     </>
   );
 };
